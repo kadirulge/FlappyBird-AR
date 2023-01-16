997daa80879823130a82ec56225327044d0e5148
kadirulge <kadirulge7@gmail.com>
2023-01-16 13:42:11 +0300

    init files

---
+++ FlappyBird-AR/Assets/GoogleARCore/Examples/AugmentedFaces/Scripts/CameraFollow.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        offset = transform.position - Player.transform.position;
+
+        
+    }
+
+    private void LateUpdate()
+    {
+        transform.position = Player.transform.position + offset;
+    }
+
+
+    void Update()
+    {
+       
+
+    }
+
+
+    public GameObject Player;
+    Vector3 offset;
+}
+
---
+++ FlappyBird-AR/Assets/_Scripts/GameOverScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverScript : MonoBehaviour
+{
+
+
+    void OnTriggerEnter(Collider col)
+    {
+
+        if (col.tag == "Ball")
+
+        {
+
+            PlayerDies();
+        }
+
+
+    }
+
+
+
+    public void PlayerDies()
+    {
+
+
+
+        UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+
+
+
+
+    }
+}
